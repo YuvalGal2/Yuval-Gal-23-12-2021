3f2ddd92975524410cff85d8bbb90a42e84c0f13
muonline.mega@gmail.com <muonline.mega@gmail.com>
2021-12-30 00:11:07 +0200

    replaced key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  weatherAppAPIKey: '76qrCG0xwAAXVpHihlalvFyZ5IpTAIVg',
+  weatherAppAPIKey: 'VkkZhr35Jxu34iELm6V0hrkdFBrqhrxU',
   useMockData: false,
   appName:'My Cool App in Prod'
 };
